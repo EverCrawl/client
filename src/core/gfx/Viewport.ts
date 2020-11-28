64085d25039fb2486775ad406d53a06344cb9213
jprochazk <honza.spacir@gmail.com>
2020-11-28 16:52:51 +0100

    Prevent browser zoom from scaling the canvas

--- src/core/gfx/Viewport.ts
+++ src/core/gfx/Viewport.ts
@@ -4,7 +4,6 @@
 export class Viewport {
     public readonly gl: WebGL2RenderingContext;
     public readonly canvas: HTMLCanvasElement;
-    /* public readonly devicePixelRatio: number = window.devicePixelRatio; */
 
     constructor(
         gl: WebGL2RenderingContext
@@ -20,8 +19,8 @@ export class Viewport {
     get height() { return this.canvas.height; }
 
     resize = () => {
-        this.canvas.width = this.canvas.clientWidth /* * this.devicePixelRatio */;
-        this.canvas.height = this.canvas.clientHeight /* * this.devicePixelRatio */;
+        this.canvas.width = this.canvas.clientWidth * window.devicePixelRatio;
+        this.canvas.height = this.canvas.clientHeight * window.devicePixelRatio;
         this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
     }
 }
