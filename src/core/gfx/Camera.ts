a5fc535ca6fc88b852095444335588dc53f6ef7b
jprochazk <honza.spacir@gmail.com>
2020-12-24 22:45:34 +0100

    changed import style

--- src/core/gfx/Camera.ts
+++ src/core/gfx/Camera.ts
@@ -1,50 +1,50 @@
 
-import * as m from "core/math";
+import { m4, Matrix4, v3, Vector3, v2, Vector2 } from "core/math";
 import { Viewport } from "./Viewport";
 
 export interface CameraOptions {
-    eye?: m.Vector3;
-    center?: m.Vector3;
+    eye?: Vector3;
+    center?: Vector3;
     near?: number;
     far?: number;
-    worldUp?: m.Vector3;
+    worldUp?: Vector3;
     zoom?: number;
 }
 
-function calcView(eye: m.Vector3, center: m.Vector3, worldUp: m.Vector3): m.Matrix4 {
-    return m.m4.lookAt(eye, center, worldUp);
+function calcView(eye: Vector3, center: Vector3, worldUp: Vector3): Matrix4 {
+    return m4.lookAt(eye, center, worldUp);
 }
 
-function calcProjection(viewport: Viewport, near: number, far: number, zoom: number): m.Matrix4 {
+function calcProjection(viewport: Viewport, near: number, far: number, zoom: number): Matrix4 {
     const hw = viewport.width / 2;
     const hh = viewport.height / 2;
-    return m.m4.orthographic(-hw, hw, -hh, hh, near, far);
+    return m4.orthographic(-hw, hw, -hh, hh, near, far);
 }
 
 export class Camera {
-    private view_: m.Matrix4;
-    private projection_: m.Matrix4;
+    private view_: Matrix4;
+    private projection_: Matrix4;
 
-    private eye_: m.Vector3;
-    private center_: m.Vector3;
+    private eye_: Vector3;
+    private center_: Vector3;
     private near_: number;
     private far_: number;
-    private worldUp_: m.Vector3;
+    private worldUp_: Vector3;
     private zoom_: number;
 
     constructor(
         public viewport: Viewport,
         options: CameraOptions = {}
     ) {
-        this.eye_ = options.eye !== undefined ? options.eye : m.v3(0, 0, -1);
-        this.center_ = options.center !== undefined ? options.center : m.v3(0, 0, 0);
+        this.eye_ = options.eye !== undefined ? options.eye : v3(0, 0, -1);
+        this.center_ = options.center !== undefined ? options.center : v3(0, 0, 0);
         this.near_ = options.near !== undefined ? options.near : -1;
         this.far_ = options.far !== undefined ? options.far : 1;
-        this.worldUp_ = options.worldUp !== undefined ? options.worldUp : m.v3(0, 1, 0);
+        this.worldUp_ = options.worldUp !== undefined ? options.worldUp : v3(0, 1, 0);
         this.zoom_ = 1;
 
-        this.projection_ = m.m4();
-        this.view_ = m.m4();
+        this.projection_ = m4();
+        this.view_ = m4();
         this.update();
         this.resize();
 
@@ -56,18 +56,18 @@ export class Camera {
         this.projection_ = calcProjection(this.viewport, this.near_, this.far_, this.zoom_);
     }
 
-    public get view(): m.Matrix4 {
+    public get view(): Matrix4 {
         return this.view_;
     }
-    public get projection(): m.Matrix4 {
+    public get projection(): Matrix4 {
         return this.projection_;
     }
-    public get position(): m.Vector2 {
-        return m.v2(this.eye_[0], this.eye_[1]);
+    public get position(): Vector2 {
+        return v2(this.eye_[0], this.eye_[1]);
     }
-    public set position(value: m.Vector2) {
-        this.eye_ = m.v3(value[0], value[1], -1);
-        this.center_ = m.v3(value[0], value[1], 0);
+    public set position(value: Vector2) {
+        this.eye_ = v3(value[0], value[1], -1);
+        this.center_ = v3(value[0], value[1], 0);
         this.update();
     }
     public get zoom(): number {
